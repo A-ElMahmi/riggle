fae8c4648d70b18376fe9eca083a31ed05c73742
Abderrahmane El Mahmi <elmahmi.a04@gmail.com>
2024-04-19 16:27:07 +0100

    Replaced x,y variables with Vectors instead

--- js/Snake.js
+++ js/Snake.js
@@ -1,47 +1,37 @@
 class Snake {
     constructor() {
-        this.x = 3
-        this.y = 2
         this.pos = createVector(floor(GRID_WIDTH / 3), floor(GRID_HEIGHT / 2))
-
         this.vel = createVector(1, 0)
-        this.velX = 1
-        this.velY = 0
-
+        
         this.tail = createVector(this.pos.x - 1, this.pos.y)
-        this.tailX = this.x - 1
-        this.tailY = this.y
     }
     
     update() {
         if (frameCount % 5 === 0) {
-            this.tailX = this.x
-            this.tailY = this.y
-
-            this.x += this.velX
-            this.y += this.velY
+            this.tail.set(this.pos)
+            this.pos.add(this.vel)
         }
         
-        this.x = constrain(this.x, 0, GRID_WIDTH-1)
-        this.y = constrain(this.y, 0, GRID_HEIGHT-1)
+        this.pos.x = constrain(this.pos.x, 0, GRID_WIDTH-1)
+        this.pos.y = constrain(this.pos.y, 0, GRID_HEIGHT-1)
     }
 
     move(dirX, dirY) {
         // Prevent backwards movement
-        if (dirX === this.velX ^ dirY === this.velY) {
+        if (dirX === this.vel.x ^ dirY === this.vel.y) {
             console.log("DIE");
         }
 
-        this.velX = dirX
-        this.velY = dirY
+        this.vel.x = dirX
+        this.vel.y = dirY
     }
 
     show() {
         noStroke()
         fill(0, 255, 255)
-        rect(this.x * GRID_SIZE, this.y * GRID_SIZE, GRID_SIZE, GRID_SIZE)
+        rect(this.pos.x * GRID_SIZE, this.pos.y * GRID_SIZE, GRID_SIZE, GRID_SIZE)
         
         fill(0, 205, 255)
-        rect(this.tailX * GRID_SIZE, this.tailY * GRID_SIZE, GRID_SIZE, GRID_SIZE)
+        rect(this.tail.x * GRID_SIZE, this.tail.y * GRID_SIZE, GRID_SIZE, GRID_SIZE)
     }
 }
