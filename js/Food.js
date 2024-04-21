63e5809c908777a53b7ab7238c8f310b04f88b08
Abderrahmane El Mahmi <elmahmi.a04@gmail.com>
2024-04-21 02:09:45 +0100

    Food colour based on value

--- js/Food.js
+++ js/Food.js
@@ -1,10 +1,11 @@
 class Food {
     constructor() {
         this.pos = createVector(floor(random(GRID_WIDTH)), floor(random(GRID_HEIGHT)))
+        this.value = ceil(random(3))
     }
 
     show() {
-        fill(254, 0, 0)
+        fill(this.value * 85, 0, 0)
         ellipse(this.pos.x * GRID_SIZE + (GRID_SIZE/2), this.pos.y * GRID_SIZE + (GRID_SIZE/2), GRID_SIZE, GRID_SIZE)
     }
 }
