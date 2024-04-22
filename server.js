85a87625de7d84cd1c2d9b2480897e4b5713f724
Abderrahmane El Mahmi <elmahmi.a04@gmail.com>
2024-04-22 14:26:41 +0100

    Added proper multiplayer mode

--- server.js
+++ server.js
@@ -1,55 +1,42 @@
-let express = require("express")
+const express = require("express")
 
-let app = express()
-let server = app.listen(3000)
+const app = express()
+const server = app.listen(3000)
 
 app.use(express.static("public"))
 
 console.log("Server is running");
 
-let io = require("socket.io")(server)
+const io = require("socket.io")(server)
+
 
 io.sockets.on("connection", (socket) => {
-    console.log("New connection :)");
+    console.log("New connection :) " + socket.id);
 
-    socket.on("move", data => {
-        socket.broadcast.emit("move", data)
+    snakes[socket.id] = { pos: newSpawnPosition() }
+
+    socket.emit("world_info", { 
+        id: socket.id,
+        gridSize: GRID_SIZE, 
+        gridWidth: GRID_WIDTH, 
+        gridHeight: GRID_HEIGHT,
+        ...snakes[socket.id].pos
+    })
+
+    socket.on("move", snake => {
+        snakes[socket.id] = snake
+
+        if (isDead(snake)) {
+            snakes[socket.id].pos = newSpawnPosition()
+            socket.emit("dead", snakes[socket.id].pos)
+        }
+
+        socket.broadcast.emit("move", snakes)
     })
 
     socket.on("disconnect", () => {
         console.log("Client disconnected :(");
+        delete snakes[socket.id]
     })
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
