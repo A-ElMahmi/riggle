ac0ef10596845aa7e74b1721d74f8978fefd31b6
Abderrahmane El Mahmi <elmahmi.a04@gmail.com>
2024-04-25 16:32:48 +0100

    Empty files

--- public/js/Food.js
+++ public/js/Food.js
@@ -8,7 +8,5 @@ class Food {
     show() {
         if (!this.pos) return
 
-        fill(this.value * 85, 0, 0)
-        ellipse(this.pos.x * this.size + (this.size/2), this.pos.y * this.size + (this.size/2), this.size, this.size)
     }
 }
